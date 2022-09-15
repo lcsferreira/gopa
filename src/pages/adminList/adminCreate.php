c1d09ea97f3394f96ddbb84454bfadbe886c2533
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-09-15 11:23:55 -0300

    Admin screen start

---
+++ src/pages/adminList/adminCreate.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Creation</title>
+</head>
+<!--
+  SQL query post admin
+  default password 
+-->
+<body>
+  <div class="container">
+    <form method="POST" action="">
+      <div class="form-input">
+        <label for="adm-name">Name</label>
+        <input type="text" placeholder="Name">
+      </div>
+      <div class="form-input">
+        <label for="adm-email">Email</label>
+        <input type="text" placeholder="Email">
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  </div>
+</body>
+</html>
---
+++ src/pages/adminList/adminList.php
@@ -0,0 +1,27 @@
+<?php
+  $title = "Admin List";                   
+  include "header.php";                 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin List</title>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>Admin List</h1>
+      <a href="/adminCreation" class="btn-create">Create</a>  
+    </div>
+    <div class="admin-list">
+     <div>
+      ADMIN INFO
+     </div>
+    </div>
+  </div>
+</body>
+</html>
