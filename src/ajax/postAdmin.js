9882f2ca0573d2c0dd39fb3fb3a481a8d2f6bbdb
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-09-27 09:01:55 -0300

    redirect after creation

--- src/ajax/postAdmin.js
+++ src/ajax/postAdmin.js
@@ -1,14 +1,23 @@
-$(function(){
-  $("#saveadmin").on('click', function(){
-      var admName = $("#adm-name").val();
-      var admEmail = $("#adm-email").val();
-      $.ajax({
-        method: "POST",
-        url:    "../../ajaxquery/saveadmin_ajax.php",
-        data: { "name": admName, "email": admEmail, "password": "123"},
-        dataType:'json',
-        type:'post',
-        contentType: 'application/x-www-form-urlencoded',
-      })
- });
+$(function () {
+  $("#saveadmin").on("click", function () {
+    var admName = $("#adm-name").val();
+    var admEmail = $("#adm-email").val();
+    $.ajax({
+      method: "POST",
+      url: "../../ajaxquery/saveadmin_ajax.php",
+      data: { name: admName, email: admEmail, password: "123" },
+      dataType: "json",
+      type: "post",
+      contentType: "application/x-www-form-urlencoded",
+      success: function (result) {
+        console.log(result);
+
+        if (result === 1) {
+          setTimeout(function () {
+            window.location.href = "../pages/adminList/adminList.php";
+          }, 2000);
+        }
+      },
+    });
+  });
 });
