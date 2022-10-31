41ceac65aefe02176262efacd7818f3859e85912
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-10-31 17:07:00 -0300

    First Access Contacts

--- src/ajaxQuerys/login/checkFirstAccess.php
+++ src/ajaxQuerys/login/checkFirstAccess.php
@@ -5,6 +5,22 @@
 //get id from url
 $id = $_GET['id'];
 
+//get the year from the id
+$year = substr($id, 0, 4);
+//get the actual year
+$actualYear = date("Y");
+if($year == $actualYear){
+  $sql = "SELECT is_active, password FROM contact WHERE id = '$id'";
+
+  $result = mysqli_query($connection, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  if($row['is_active'] == 0){
+  //redirect to login page
+  header("Location: ../../pages/login/login.php");
+}
+}
+
 $sql = "SELECT is_active, password FROM admin WHERE id = '$id'";
 
 $result = mysqli_query($connection, $sql);
