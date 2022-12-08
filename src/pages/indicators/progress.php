b395bff93c758cd44de2dc74445060b65546e933
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-12-08 14:49:59 -0300

    starting progress logic

--- src/pages/indicators/progress.php
+++ src/pages/indicators/progress.php
@@ -5,6 +5,44 @@
 <?php
   //get id from url
   $id = $_GET['id'];
+  //get indicators_step from country table
+  $sql = "SELECT indicators_step FROM country WHERE id = $id";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  if($row['indicators_step'] == "not started"){
+    //create new row in demographic_values table
+    $sql = "INSERT INTO demographic_values";
+    $result = mysqli_query($conn, $sql);
+    $id_demographic_values = mysqli_insert_id($conn);
+    //create new row in demographic_comments table
+    $sql = "INSERT INTO demographic_comments";
+    $result = mysqli_query($conn, $sql);
+    $id_demographic_comments = mysqli_insert_id($conn);
+
+    //create new row in demoind_country_comments table
+    $sql = "INSERT INTO demoind_country_comments (id_demo_value, id_demo_comment, id_country) VALUES ($id_demographic_values, $id_demographic_comments, $id)";
+    $result = mysqli_query($conn, $sql);
+
+    //update indicators_step in country table
+    $sql = "UPDATE country SET indicators_step = 'started' WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+  }
+
+
+  $sql = "SELECT * FROM demoind_country_comments WHERE id = $id";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  $id_values = $row['id_demo_value'];
+  $id_comments = $row['id_demo_comment'];
+
+  $sql = "SELECT total_indicators FROM demographic_values WHERE id = $id_values";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_assoc($result);
+
+  $total_demographic_indicators = $row['total_indicators'];
+  $demoind_percent = ($total_demographic_indicators / 9) * 100;
 ?>
 <?php
   if(!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== true){
