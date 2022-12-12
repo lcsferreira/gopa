94e6e76d3196f68c6031a83150395933c3885a04
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-12-12 14:53:28 -0300

    loading values from table

--- src/pages/indicators/demographic.php
+++ src/pages/indicators/demographic.php
@@ -5,6 +5,18 @@
 <?php
   //get id from url
   $id = $_GET['id'];
+  //select row from demographic_values_admin table
+  $sql = "SELECT * FROM demographic_values_admin WHERE id = $id";
+  $result = mysqli_query($connection, $sql);
+  $admin_values = mysqli_fetch_assoc($result);
+  //select row from demographic_comments table
+  $sql = "SELECT * FROM demographic_comments WHERE id = $id";
+  $result = mysqli_query($connection, $sql);
+  $comments = mysqli_fetch_assoc($result);
+  //select row from demographic_values_contact table
+  $sql = "SELECT * FROM demographic_values_contact WHERE id = $id";
+  $result = mysqli_query($connection, $sql);
+  $contact_values = mysqli_fetch_assoc($result);
 ?>
 <?php
   if(!isset($_SESSION['loggedin']) && $_SESSION['loggedin'] !== true){
@@ -34,43 +46,182 @@
       <div class="indicators">
         <div class="form-input">
           <label for="country">Country</label>
-          <input type="text" name="country" id="country">
+          <input type="text" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['country'];
+              }
+            ?> 
+            name="country" id="country-admin"
+          >
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="country-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="country">Country</label>
+          <input type="text"
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['country'];
+              } 
+            ?> 
+          name="country" id="country">
+        </div>
+        <textarea placeholder="Add a comment..." name="comments" id="country-comments" cols="30" rows="5" class="comment"
+              <?php
+                if($comments['country'] != null){
+                  echo "value=" . $comments['country'];
+                }
+              ?>
+        ></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="capital">Capital</label>
-          <input type="text" name="capital" id="capital">
+          <input type="text" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['capital'];
+              }
+            ?>
+            name="capital" id="capital-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="capital-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="capital">Capital</label>
+          <input type="text" <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['capital'];
+              } 
+            ?> name="capital" id="capital">
+        </div>
+        <textarea placeholder="Add a comment..." 
+              <?php
+                if($comments['country'] != null){
+                  echo "value=" . $comments['capital'];
+                }
+              ?>
+              name="comments" id="capital-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="total-population">Total population (number of people)
             <p>Inhabits of the country</p>
           </label>
-          <input type="number" name="total-population" id="total-population">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['total_population'];
+              }
+            ?>
+            name="total-population" id="total-population-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="total-population-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="total-population">Total population (number of people)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['total_population'];
+              } 
+            ?>
+            name="total-population" id="total-population">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['total_population'];
+            }
+          ?>
+          name="comments" id="total-population-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="urban-population">Urban population (%)
             <p>Percentage (%) of the total population living in urban areas</p>  
           </label>
-          <input type="number" name="urban-population" id="urban-population">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['urban_population'];
+              }
+            ?>
+            name="urban-population" id="urban-population-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="urban-population-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="urban-population">Urban population (%)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['urban_population'];
+              } 
+            ?>
+            name="urban-population" id="urban-population">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['urban_population'];
+            }
+          ?>
+          name="comments" id="urban-population-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="life-expentacy">Life expentacy (years)
             <p>Average age that a person of the population is expected to live</p>
           </label>
-          <input type="number" name="life-expentacy" id="life-expentacy">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['life_expentacy'];
+              }
+            ?>
+            name="life-expentacy" id="life-expentacy-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="life-expentacy-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="life-expentacy">Life expentacy (years)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['life_expentacy'];
+              } 
+            ?>
+            name="life-expentacy" id="life-expentacy">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['life_expentacy'];
+            }
+          ?>
+          name="comments" id="life-expentacy-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
@@ -79,27 +230,110 @@
             0: perfect equality; 1: perfect inequality
             </p>
           </label>
-          <input type="number" name="gini-index" id="gini-index">
+          <input type="number"
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['gini_index'];
+              }
+            ?> 
+            name="gini-index" id="gini-index-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="gini-index-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="gini-index">Gini inequality index (number between 0 and 1)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['gini_index'];
+              } 
+            ?>
+            name="gini-index" id="gini-index">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['gini_index'];
+            }
+          ?>
+          name="comments" id="gini-index-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="human-index">Human development index (number between 0 and 1)
             <p>Summary measure of average achievement in key dimensions of human development: a long and healthy life, being knowledgeable and have a decent standard of living</p>
           </label>
-          <input type="number" name="human-index" id="human-index">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['human_index'];
+              }
+            ?>
+            name="human-index" id="human-index-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="human-index-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="human-index">Human development index (number between 0 and 1)</label>
+          <input type="number"
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['human_index'];
+              } 
+            ?>
+            name="human-index" id="human-index">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['human_index'];
+            }
+          ?>
+          name="comments" id="human-index-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
           <label for="literacy-rate">Literacy rate (%)
             <p>Percentage (%) of adults aged 15 and older who can both read and write</p>
           </label>
-          <input type="number" name="literacy-rate" id="literacy-rate">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['literacy_rate'];
+              }
+            ?>
+            name="literacy-rate" id="literacy-rate-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="literacy-rate-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="literacy-rate">Literacy rate (%)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['literacy_rate'];
+              } 
+            ?>
+            name="literacy-rate" id="literacy-rate">
+        </div>
+        <textarea placeholder="Add a comment..."
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['literacy_rate'];
+            }
+          ?> name="comments" id="literacy-rate-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="indicators">
         <div class="form-input">
@@ -107,9 +341,37 @@
             <p>Percentage (%) of deaths by NCDs (include cancer, diabetes mellitus, cardiovascular diseases, digestive diseases, skin diseases, musculoskeletal diseases, and congenital anomalies)
             </p>
           </label>
-          <input type="number" name="deaths-diseases" id="deaths-diseases">
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] != "admin"){
+                echo "disabled";
+              }
+              if($admin_values['country'] != null){
+                echo "value=" . $admin_values['deaths_diseases'];
+              }
+            ?>
+            name="deaths-diseases" id="deaths-diseases-admin">
         </div>
-        <textarea placeholder="Add a comment..." name="comments" id="deaths-diseases-comments" cols="30" rows="5" class="comment"></textarea>
+        <div class="form-input">
+          <label for="deaths-diseases">Deaths due to non-communicable diseases (%)</label>
+          <input type="number" 
+            <?php
+              if($_SESSION['userType'] == "admin"){
+                echo "disabled";
+              }
+              if($contact_values['country'] != null){
+                echo "value=" . $contact_values['deaths_diseases'];
+              } 
+            ?>
+            name="deaths-diseases" id="deaths-diseases">
+        </div>
+        <textarea placeholder="Add a comment..." 
+          <?php
+            if($comments['country'] != null){
+              echo "value=" . $comments['deaths_diseases'];
+            }
+          ?>
+          name="comments" id="deaths-diseases-comments" cols="30" rows="5" class="comment"></textarea>
       </div>
       <div class="buttons">
         <button class="btn-back" type="button" <?php
--- src/pages/indicators/progress.php
+++ src/pages/indicators/progress.php
@@ -6,8 +6,8 @@
   //get id from url
   $id = $_GET['id'];
   //get indicators_step from country table
-  $sql = "SELECT indicators_step FROM country WHERE id = $id";
-  $result = mysqli_query($conn, $sql);
+  $sql = "SELECT indicators_step FROM countries WHERE id = $id";
+  $result = mysqli_query($connection, $sql);
   $row = mysqli_fetch_assoc($result);
 
   $demographic_progress = 0;
@@ -15,23 +15,27 @@
   if($row['indicators_step'] == "not started"){
     //create row in demographic_comments, demographic_values_admin and demograpghic_values_contact tables
     $sql = "INSERT INTO demographic_comments (id) VALUES ($id)";
-    mysqli_query($conn, $sql);
+    mysqli_query($connection, $sql);
     //get the id of the last row inserted
     $id_comment = $id;
     $sql = "INSERT INTO demographic_values_admin (id) VALUES ($id)";
-    mysqli_query($conn, $sql);
+    mysqli_query($connection, $sql);
     $id_value_admin = $id;
     $sql = "INSERT INTO demographic_values_contact (id) VALUES ($id)";
-    mysqli_query($conn, $sql);
+    mysqli_query($connection, $sql);
     $id_value_contact = $id;
 
     //create row in demoind_country_comments, with all foreign keys from id, id_value_admin, id_comment, id_value_contact
-    $sql = "INSERT INTO demoind_country_comments (id_country, id_value_admin, id_comment, id_value_contact) VALUES ($id, $id_comment, $id_value_admin, $id_value_contact)";
+    $sql = "INSERT INTO demoind_country_comment (id_country, id_value_admin, id_comment, id_value_contact) VALUES ($id, $id_comment, $id_value_admin, $id_value_contact)";
+    mysqli_query($connection, $sql);
+
+    //update indicators_step to "in progress" in countries table
+    $sql = "UPDATE countries SET indicators_step = 'started' WHERE id = $id";
   }
 
   //select row from demographic_values_contacct table
   $sql = "SELECT * FROM demographic_values_contact WHERE id = $id";
-  $result = mysqli_query($conn, $sql);
+  $result = mysqli_query($connection, $sql);
   $row = mysqli_fetch_assoc($result);
   //for each row except id, check if the value is not null
   foreach($row as $key => $value){
